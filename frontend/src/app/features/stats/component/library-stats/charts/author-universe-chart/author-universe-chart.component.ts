665fac0350cdad846f272c7ad044ac06ded182cd
Alex <25013571+alexhb1@users.noreply.github.com>
2026-08-02 14:09:52 +0100

    refactor(stats): isolate author universe presentation

--- frontend/src/app/features/stats/component/library-stats/charts/author-universe-chart/author-universe-chart.component.ts
+++ frontend/src/app/features/stats/component/library-stats/charts/author-universe-chart/author-universe-chart.component.ts
@@ -1,6 +1,4 @@
 import {Component, computed, DestroyRef, inject, input} from '@angular/core';
-import { StatsChartJsHostDirective } from '../../../shared/stats-chart-js-host.directive';
-
 import {TranslocoDirective, TranslocoService} from '@jsverse/transloco';
 import {Chart, ChartConfiguration, ChartData, TooltipModel} from 'chart.js';
 import {BaseChartDirective} from 'ng2-charts';
@@ -42,7 +40,6 @@ const COMPLETION_COLORS = {
 @Component({
   selector: 'app-author-universe-chart',
   standalone: true,
-  hostDirectives: [StatsChartJsHostDirective],
   imports: [BaseChartDirective, TranslocoDirective],
   templateUrl: './author-universe-chart.component.html',
   styleUrls: ['./author-universe-chart.component.scss'],
